ce1e03e8aa5247d11e95e9b2eef108dee421362d
badiou <ourobadiou@gmail.com>
2024-08-07 00:15:49 +0200

    update terraform

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -1,8 +1,8 @@
 # Azure GUIDS
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "subscription_id" { default ="c6b49f87-b44b-4f50-9328-64efe17053d2"}
+variable "client_id" {default ="11958eb2-8495-4c38-b1e9-96fd3a42fb29"}
+variable "client_secret" {default ="W3s8Q~DI0ZNUZ6eTDX4OcllNbHGIrz5UCEPHRbOh"}
+variable "tenant_id" {default ="1d4ecdae-1850-4f9b-8f09-aedca77aa0f1"}
 
 # Resource Group/Location
 variable "location" {
