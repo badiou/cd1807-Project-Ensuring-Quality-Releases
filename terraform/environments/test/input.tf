4a26af02a718a58c236fa0b53c53b69143ad9141
badiou <ourobadiou@gmail.com>
2024-08-06 23:43:32 +0200

    update terraform

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -11,7 +11,9 @@ variable "location" {
 variable "resource_group" {
      default     = "AzuredevopsRG"
 }
-variable "application_type" {}
+variable "application_type" {
+  default = "myapplication-ourobadiou"
+}
 
 # Network
 variable virtual_network_name {}
@@ -19,5 +21,9 @@ variable address_prefix_test {
   default= ["10.5.1.0/24"]
 }
 variable address_space {
+  default =  ["10.5.0.0/16"]
+}
+
+variable application_type {
   default =  ["10.5.0.0/16"]
 }
