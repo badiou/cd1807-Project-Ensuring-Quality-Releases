5db5f34deda306ff193b6f42778130ea7aca68de
badiou <ourobadiou@gmail.com>
2024-08-11 16:01:59 +0200

    subnet fixes

--- terraform/environments/test/configure-tfstate-storage-account.sh
+++ terraform/environments/test/configure-tfstate-storage-account.sh
@@ -16,6 +16,7 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
 export ARM_ACCESS_KEY=$ACCOUNT_KEY
 
+
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --account-key $ACCOUNT_KEY
 echo "RESOURCE_GROUP_NAME=$RESOURCE_GROUP_NAME"
