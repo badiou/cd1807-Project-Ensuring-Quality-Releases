1424f76b541b70f7dbfcec479b9fd2aa8dcaa501
badiou <ourobadiou@gmail.com>
2024-08-11 14:43:26 +0200

    subnet fixes

--- terraform/modules/publicip/input.tf
+++ terraform/modules/publicip/input.tf
@@ -4,3 +4,5 @@ variable "resource_group" {}
 
 variable "application_type" {}
 variable "resource_type" {}
+
+variable "public_ip_address_id" {}
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_interface" "test" {
-  name                = "udacity-nic"
+  name                = "${var.application_type}-nic"
   location            = var.location
   resource_group_name = var.resource_group
 
@@ -21,10 +21,10 @@ resource "azurerm_linux_virtual_machine" "test" {
   disable_password_authentication = false
   network_interface_ids = [azurerm_network_interface.test.id]
 
-  # admin_ssh_key {
-  #   username   = "ourobadiou"
-  #   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEL7hBFBLjSOBQKNXZl7zjOTIMa+YCmXrq18P9FKvCKF19ZqAVMrWYnaplO6JU7xegaevVkUvFcoRfA9X3NljZrajFS+aBUzr/wUkndDUv/HAkYxguyuURm1o7WP+GVc5ov4klVmpRtobNGj5qVLzswI4r7DuwQbsg9HxBSMx5m5cWl1hYb4WmWX487WMpO4ISmPKF4N76h7d08PNY51S//LQeAWNV6hRNInvjnYthR9pdmvR5urh5LKPXAtZsSl62dgds2uyhn12kbKMxGnimTgfYesBAaHAovlFx/I8dcSNuvMEbvWXGa7e6kMrgUnwnc84t3Sr039ur7Z8t47g7UEbTqX5XbrMPLLPsokvvk2GB2VAyvtir2xJg1elSzyOvSZo0n+FlV7Pjvhw2ge+v6hNe0kzGuTwmyOe1VGtqvo/yTDictLV/dR5YWLSMeRp7CJYrKaKl1dmw92QdF/Et2XXmnom2EaEnMcoyU0/JOURdJWoTEU+6Pw3KTJuLcN8="
-  # }
+  admin_ssh_key {
+    username   = "ourobadiou"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDEL7hBFBLjSOBQKNXZl7zjOTIMa+YCmXrq18P9FKvCKF19ZqAVMrWYnaplO6JU7xegaevVkUvFcoRfA9X3NljZrajFS+aBUzr/wUkndDUv/HAkYxguyuURm1o7WP+GVc5ov4klVmpRtobNGj5qVLzswI4r7DuwQbsg9HxBSMx5m5cWl1hYb4WmWX487WMpO4ISmPKF4N76h7d08PNY51S//LQeAWNV6hRNInvjnYthR9pdmvR5urh5LKPXAtZsSl62dgds2uyhn12kbKMxGnimTgfYesBAaHAovlFx/I8dcSNuvMEbvWXGa7e6kMrgUnwnc84t3Sr039ur7Z8t47g7UEbTqX5XbrMPLLPsokvvk2GB2VAyvtir2xJg1elSzyOvSZo0n+FlV7Pjvhw2ge+v6hNe0kzGuTwmyOe1VGtqvo/yTDictLV/dR5YWLSMeRp7CJYrKaKl1dmw92QdF/Et2XXmnom2EaEnMcoyU0/JOURdJWoTEU+6Pw3KTJuLcN8="
+  }
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
