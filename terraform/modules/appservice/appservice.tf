ccdda2b2fa37b69bcc38aaeeca6103fc5d5b3aaa
badiou <ourobadiou@gmail.com>
2024-08-23 18:09:25 +0200

    Update pipeline

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,12 +1,9 @@
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
   resource_group_name = var.resource_group
-
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Linux" 
+  sku_name            = "Free"
 }
 
 
